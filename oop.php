b1b8cffd218e1027ab4e0e7df32e797afa4c7b4e
shahriarshawon01 <shahriarshawon001@gmail.com>
2025-01-04 23:33:55 +0600

    module 5 project done

---
+++ oop.php
@@ -0,0 +1,76 @@
+<?php
+
+class Book
+{
+    public $title;
+    public $availableCopies;
+
+    public function __construct($title, $availableCopies)
+    {
+        $this->title = $title;
+        $this->availableCopies = $availableCopies;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getAvailableCopies()
+    {
+        return $this->availableCopies;
+    }
+
+    public function borrowBook()
+    {
+        if ($this->availableCopies > 0) {
+            $this->availableCopies--;
+        } else {
+            echo "No copies available for '{$this->title}'\n";
+        }
+    }
+
+    public function returnBook()
+    {
+        $this->availableCopies++;
+    }
+}
+
+class Member
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function borrowBook($book)
+    {
+        $book->borrowBook();
+        echo "{$this->name} borrowed '{$book->getTitle()}'\n";
+    }
+
+    public function returnBook($book)
+    {
+        $book->returnBook();
+        echo "{$this->name} returned '{$book->getTitle()}'\n";
+    }
+}
+
+$book1 = new Book('The Great Gatsby', 5);
+$book2 = new Book('To Kill a Mockingbird', 3);
+
+$member1 = new Member('John Doe');
+$member2 = new Member('Jane Smith');
+
+$member1->borrowBook($book1);
+$member2->borrowBook($book2);
+
+echo "Available Copies of '{$book1->getTitle()}': {$book1->getAvailableCopies()}\n";
+echo "Available Copies of '{$book2->getTitle()}': {$book2->getAvailableCopies()}\n";
